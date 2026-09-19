6962094ac5e0252fc9b049478ca102fa82178c6e
grotshteyn <148748991+grotshteyn@users.noreply.github.com>
2026-09-19 11:45:47 +0200

    Fix malformed app import newline

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import { applyRulesAfterImport } from "./rule-import-orchestrator.js";\nimport { applyAutomaticRules } from "./rule-service.js";
+import { applyRulesAfterImport } from "./rule-import-orchestrator.js";
+import { applyAutomaticRules } from "./rule-service.js";
 import { allocateTransaction, applyPartnerRuleToExistingTransactions, createManualPlanMatch, createPartnerRule, unallocateTransaction } from "./rule-actions.js";
 
 const config = window.PYBUDGET_CONFIG || {};
